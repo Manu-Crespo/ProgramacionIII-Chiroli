52fbb9c66b700070ee5ce85c76adf121e2bda457
Manu-Crespo <emmanuelcrespo_90@hotmail.com>
2025-11-15 17:09:04 -0300

    terminando

--- Portafolio/portfolio-astro/index.js
+++ Portafolio/portfolio-astro/index.js
@@ -1,3 +1,38 @@
 document.getElementById("scroll").addEventListener("click", () => {
       window.scrollTo({ top: 0, behavior: "smooth" });
     });
+
+  const form = document.getElementById("contact-form");
+  const messageBox = document.getElementById("form-message");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    emailjs.sendForm("service_4dim1yw", "template_ousz0q6", this)
+      .then(() => {
+        // Mensaje bonito
+        messageBox.textContent = "¡Mensaje enviado con éxito!";
+        messageBox.className = "success";
+
+        // Limpiar formulario
+        form.reset();
+
+        // Ocultar mensaje después de unos segundos
+        setTimeout(() => {
+          messageBox.className = "";
+          messageBox.textContent = "";
+        }, 4000);
+      })
+      .catch(() => {
+        messageBox.textContent = "Hubo un error al enviar el mensaje. Intenta nuevamente.";
+        messageBox.className = "error";
+
+        setTimeout(() => {
+          messageBox.className = "";
+          messageBox.textContent = "";
+        }, 4000);
+      });
+  });
+
+
+
